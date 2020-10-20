2542f7b0a6cda7a30789c504a7f535c1d0e50995
Rodrigo Ferreira <rodrigo.ferreira@mobly.com.br>
2020-10-20 14:59:47 -0300

    added methods to return validation errors on response

--- app/Exceptions/Repository/RequestNotValidException.php
+++ app/Exceptions/Repository/RequestNotValidException.php
@@ -2,8 +2,25 @@
 namespace App\Exceptions\Repository;
 
 use CodeIgniter\Exceptions\ExceptionInterface;
+use phpDocumentor\Reflection\Types\Iterable_;
+use phpDocumentor\Reflection\Types\This;
 
 class RequestNotValidException extends \RuntimeException implements ExceptionInterface
 {
+    protected $errors;
 
+    public function setErrors (Iterable $errors) : void
+    {
+        $this->errors = $errors;
+    }
+
+    public function getErrors(): Iterable
+    {
+        return $this->errors ?? [];
+    }
+
+    public function hasErrors(): bool
+    {
+        return $this->errors ? true : false;
+    }
 }
