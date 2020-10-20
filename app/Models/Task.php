d1096921a313ccdb9f6ec8317d252fe8aa9a5eb8
Rodrigo Ferreira <rodrigo.ferreira@mobly.com.br>
2020-10-20 01:31:14 -0300

    created TaskModel

---
+++ app/Models/Task.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Task extends Model
+{
+    protected $table = "tarefas";
+    protected $primaryKey = "id";
+    protected $allowedFields = ['descricao', 'data', 'finalizacao', 'responsavel', 'categoria'];
+}
