7dd8862f85a7c575356f614b422d1182a1f92183
Rodrigo Ferreira <rodrigo.ferreira@mobly.com.br>
2020-10-20 14:58:59 -0300

    added config rules to enable validation

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -30,7 +30,45 @@ class Validation
 		'single' => 'CodeIgniter\Validation\Views\single',
 	];
 
-	//--------------------------------------------------------------------
-	// Rules
-	//--------------------------------------------------------------------
+    //--------------------------------------------------------------------
+    // Rules
+    //--------------------------------------------------------------------
+    public $task = [
+        "descricao" => [
+            "rules" => "required|string|max_length[140]|min_length[5]",
+            "errors" => [
+                "required" => "A tarefa precisa de uma descrição",
+                "string" => "A descrição precisa ser uma 'string', texto entre aspas",
+                "max_length" => "Tamanho mínimo é de 5 caractéres",
+                "min_length" => "Tamanho máximo é de 140 caractéres"
+            ]
+        ],
+        "responsavel" => [
+            "rules" => "required|is_natural",
+            "errors" => [
+                "required" => "Deve haver um responsável associado á tarefa",
+                "is_natural" => "Precisa ser um valor numérico, que identifique um responsável cadastrado"
+            ]
+        ],
+        "data" => [
+            "rules" => "required|valid_date",
+            "errors" => [
+                "required" => "Este campo é obrigatóiro",
+                "valid_date" => "Deve conter uma data válida",
+            ]
+        ],
+        "finalizacao" => [
+            "rules" => "permit_empty|valid_date",
+            "errors" => [
+                "valid_date" => "Deve conter uma data válida",
+            ]
+        ],
+        "categoria" => [
+            "rules" => "required|is_natural",
+            "errors" => [
+                "required" => "A tarefa precisa estar associada á alguma categoria",
+                "is_natural" => "Precisa ser um valor numérico, que represente uma categoria válida"
+            ]
+        ]
+    ];
 }
