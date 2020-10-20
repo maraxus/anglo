f6f890a5cb12b438b90e1a7c4b05da6be4c769e5
Rodrigo Ferreira <rodrigo.ferreira@mobly.com.br>
2020-10-20 14:35:25 -0300

    pointed to validation rules in config

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,5 @@ class Task extends Model
     protected $table = "tarefas";
     protected $primaryKey = "id";
     protected $allowedFields = ['descricao', 'data', 'finalizacao', 'responsavel', 'categoria'];
+    protected $validationRules = 'task';
 }
